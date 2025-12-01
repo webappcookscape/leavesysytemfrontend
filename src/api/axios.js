9261b341d820d718120166069f76b820dceeb4ad
webappcookscape <webapp.cookscape@google.com>
2025-12-01 14:58:55 +0530

    apispacefix

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://leavesysytembackend.onrender.com/api ",
+  baseURL: "https://leavesysytembackend.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
